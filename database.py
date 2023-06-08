d96e4e4a5d71e092c371c0d405679dc0b9d51d68
minjkang <minjkang0901@naver.com>
2023-06-09 01:05:02 +0900

    commit

--- database.py
+++ database.py
@@ -1,21 +1,60 @@
 from pymongo import MongoClient
-from model import User, User_login
+from model import User, UserLogin, UserCreate
+from passlib.context import CryptContext
+import bcrypt
 import pydantic
 from bson import ObjectId
 pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class MongoDB:
     def __init__(self, uri: str, db_name: str):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
+        self.users_collection = self.db['users'] 
     
     def load_user(self):
         user_data = self.db['users'].find()
         user_list = []
         for item in user_data:
+            item["_id"] = str(item["_id"])
             user_list.append(item)
         return user_list
+    
+    def load_user_id(self, user_id: str): 
+        user_data = self.db['users'].find_one({'id': user_id}) 
+        return user_data
+    
+    def load_user_userID(self, user_id: int):
+        user_data = self.db['users'].find_one({'userId': user_id})
+        return user_data
+    
+    def create_user(self, user_name: str, user_age: int, user_gender: str, user_email: str, user_id: str, hashed_password: str, user_size: object):
+        userId = self.db['users'].estimated_document_count() + 1
+        user_data = {
+            "userId": userId,
+            "userName": user_name, 
+            "age": user_age, 
+            "gender": user_gender, 
+            "email": user_email, 
+            "id": user_id, 
+            "password": hashed_password, 
+            "shoesSizes": user_size 
+        }
+        self.users_collection.insert_one(user_data)
 
+    def load_username(self, username: str):
+        user_data = self.db['users'].find_one({'userName': username})
+        return user_data
+
+    def hash_password(self, password: str) -> str:
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return hashed_password.decode("utf-8")
+    
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+    
     def check_user_cred(self, username: str, password: str) -> bool:
         return bool(self.db.users.find_one({'username': username, 'password': password}))
 
@@ -37,30 +76,3 @@ class MongoDB:
         print(user_data)
         
         return user_data
-
-# # MongoDB 연결
-# client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://root:password@mongodb:27017')
-# db = client.HGOO
-# collection_users = db.users
-
-
-# # password 암호화 모듈
-# from passlib.context import CryptContext
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-# # password 비교
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
-
-
-# # password 암호화
-# def hash_password(password):
-#     return pwd_context.hash(password)
-
-
-# # DB 에서 User 정보 가져오기
-# async def get_user_info(user_email: str):
-#     user_info = await collection_users.find_one({"email":user_email})
-#     if user_info:
-#         return user_info
--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from typing import Optional, Set
 from pydantic import BaseModel, BaseConfig, Field
 
-
 class Size(BaseModel):
     nike: int
     adidas: int
@@ -10,21 +9,23 @@ class Size(BaseModel):
     converse: int
 
 class User(BaseModel):
-    _id: str = None
     userId: int = Field(title="user_id")
     userName: str = Field(title="사용자 이름")
     age: int = Field(title="사용자 나이")
     gender: str = Field(title="사용자 성별")
     email: str = Field(title="사용자 이메일")
     id: str = Field(title="사용자 아이디")
-    password: str = Field(title="사용자 비밀번호")
     shoesSizes: Optional[Size] = None
 
-class User_login(BaseModel):
-    userId: int = Field(title="user_id")
+class UserLogin(BaseModel):
+    id: str = Field(title="사용자 아이디")
+    password: str = Field(title="사용자 비밀번호")
+
+class UserCreate(BaseModel):
     userName: str = Field(title="사용자 이름")
     age: int = Field(title="사용자 나이")
     gender: str = Field(title="사용자 성별")
     email: str = Field(title="사용자 이메일")
     id: str = Field(title="사용자 아이디")
     password: str = Field(title="사용자 비밀번호")
+    shoesSizes: Optional[object] = None
